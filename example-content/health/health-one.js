34a0779cc446e722fe4d01dc6a286d061a5cadad
Kevin Haggerty <kevin.haggerty@havasww.com>
2016-03-09 11:07:41 -0500

    adding test coverage views and example content

---
+++ example-content/health/health-one.js
@@ -0,0 +1,8 @@
+module.exports = {
+	view: "index",
+	title: "Health",
+	content:{
+		title: "Text for Health page",
+		content:"There are several ways to take data (a string of code) and run it as part of the current program.The most obvious way is the special operator eval, which will execute a string of code in the current scope. This is usually a bad idea because it breaks some of the sane properties that scopes normally have, such as being isolated from the outside world."
+	}
+}
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var navigationData = {};
 var handler = function(req, res) {
     // Issue with this object is defined as the content object
     // instead of the the binding elems from the addRoutes function
-    console.log('handing a page request:', allPaths);
+    // console.log('handing a page request:', allPaths);
     res.render(this.view, {
         content: this.content
     });
@@ -26,13 +26,13 @@ var addroute = function(path, hdlr) {
 var addRoutes = function(contentObj, parentDir) {
     // console.log('content element:', contentObj);
     _.each(contentObj, function(pageData, pathFragment) {
-        console.log('page content:',pageData, pathFragment);
+        // console.log('page content:',pageData, pathFragment);
         // only test we need is if the content object exists
         if (pageData.content) {
             var dirflPath = basePath + (parentDir ? parentDir : "") + pathFragment + '.html';
             addroute(dirflPath, handler.bind(pageData));    
         } else {
-            console.log('adding sub routes:', pathFragment, pageData);
+            // console.log('adding sub routes:', pathFragment, pageData);
             addRoutes(pageData, pathFragment + "/");
         }
     });
@@ -45,7 +45,7 @@ module.exports = function(contentDir, options){
     options = options || {};
     var content = require('require-dir')(contentDir, {recurse: true});
 
-    console.log('raw content:', content);
+    // console.log('raw content:', content);
     var ourRouter = addRoutes(content);
     if (options.sitemap) {
         ourRouter.get("/sitemap", handler.bind({view: options.sitemap, content: {sitemap: allPaths}}));    
